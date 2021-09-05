5b49097a6c3ac968755d37ac6a6b21ca3a8a09f9
zingxy <1067577595@qq.com>
2021-09-05 20:57:32 +0800

    add function api

--- src/display.ts
+++ src/display.ts
@@ -1,7 +1,7 @@
 // 后端请求接口
 interface RawData {
-  name: string; // egg Chongqing, NanChang.
-  label: string; // indicate which type this entity is. egg LOC, PER, ORG
+  name: string; // entity's name. egg Chongqing, NanChang.
+  label: string; // indicate which type this entity is. egg LOC, PER, ORG.
   count?: number; // how many times that identical entity occur in text.
 }
 
@@ -15,8 +15,6 @@ interface Entity {
 
 const text =
   "9790663	Kinetic analysis and X-ray structure of haloalkane dehalogenase with a modified halide-binding site.	Haloalkane dehalogenase (DhlA) catalyzes the hydrolysis of haloalkanes via an alkyl-enzyme intermediate. Trp175 forms a halogen/halide-binding site in the active-site cavity together with Trp125. To get more insight in the role of Trp175 in DhlA, we mutated residue 175 and explored the kinetics and X-ray structure of the Trp175Tyr enzyme. The mutagenesis study indicated that an aromatic residue at position 175 is important for the catalytic performance of DhlA. Pre-steady-state kinetic analysis of Trp175Tyr-DhlA showed that the observed 6-fold increase of the Km for 1,2-dibromoethane (DBE) results from reduced rates of both DBE binding and cleavage of the carbon-bromine bond. Furthermore, the enzyme isomerization preceding bromide release became 4-fold faster in the mutant enzyme. As a result, the rate of hydrolysis of the alkyl-enzyme intermediate became the main determinant of the kcat for DBE, which was 2-fold higher than the wild-type kcat. The X-ray structure of the mutant enzyme at pH 6 showed that the backbone structure of the enzyme remains intact and that the tyrosine side chain lies in the same plane as Trp175 in the wild-type enzyme. The Clalpha-stabilizing aromatic rings of Tyr175 and Trp125 are 0.7 A further apart and due to the smaller size of the mutated residue, the volume of the cavity has increased by one-fifth. X-ray structures of mutant and wild-type enzyme at pH 5 demonstrated that the Tyr175 side chain rotated away upon binding of an acetic acid molecule, leaving one of its oxygen atoms hydrogen bonded to the indole nitrogen of Trp125 only. These structural changes indicate a weakened interaction between residue 175 and the halogen atom or halide ion in the active site and help to explain the kinetic changes induced by the Trp175Tyr mutation.";
-
-const names = ["Trp175Tyr"];
 
 // function position(text: string, names: Array<string>): Array<Entity> {
 //   // let expression = /name/
@@ -38,6 +36,41 @@ const names = ["Trp175Tyr"];
 //   }
 //   return entitys;
 // }
+function api(text: string): Array<RawData | Entity> {
+  // post client data to server, and get feedback.
+  const server = "";
+  const entitys: Array<RawData | Entity> = [
+    {
+      name: "Trp175Tyr",
+      label: "snp",
+    },
+    {
+      name: "Kinetic",
+      label: "dna",
+    },
+    {
+      name: "structure",
+      label: "protein",
+    },
+  ];
+  return entitys;
+}
+
+function handler(e: Event) {
+  let text = (document.getElementById("input") as HTMLInputElement).value;
+  const entitys = api(text);
+  render(entitys, text);
+  console.log(entitys);
+  e.preventDefault(); // forbid default behavial.
+}
+
+function main() {
+  let btn = document.getElementById("submit");
+  if (btn) {
+    btn.addEventListener("click", handler);
+    // btn.addEventListener("click", ()=>{});
+  }
+}
 
 function render(entitys: Array<RawData | Entity>, text: string) {
   let template = text;
@@ -51,24 +84,6 @@ function render(entitys: Array<RawData | Entity>, text: string) {
   if (root) {
     root.innerHTML = template;
   }
-  console.log(template);
 }
 
-// const entitys = position(text, names);
-// const entitys: RawData =
-
-const entitys: Array<RawData | Entity> = [
-  {
-    name: "Trp175Tyr",
-    label: "snp",
-  },
-  {
-    name: "Kinetic",
-    label: "dna",
-  },
-  {
-    name: "structure",
-    label: "protein",
-  },
-];
-render(entitys, text);
+main();
