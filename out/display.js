56839545b16f54445e2d056c0908be0d00f4c56d
zingxy <1067577595@qq.com>
2021-09-07 15:12:26 +0800

    fetch api

--- out/display.js
+++ out/display.js
@@ -1,29 +1,50 @@
 "use strict";
-const text = "9790663	Kinetic analysis and X-ray structure of haloalkane dehalogenase with a modified halide-binding site.	Haloalkane dehalogenase (DhlA) catalyzes the hydrolysis of haloalkanes via an alkyl-enzyme intermediate. Trp175 forms a halogen/halide-binding site in the active-site cavity together with Trp125. To get more insight in the role of Trp175 in DhlA, we mutated residue 175 and explored the kinetics and X-ray structure of the Trp175Tyr enzyme. The mutagenesis study indicated that an aromatic residue at position 175 is important for the catalytic performance of DhlA. Pre-steady-state kinetic analysis of Trp175Tyr-DhlA showed that the observed 6-fold increase of the Km for 1,2-dibromoethane (DBE) results from reduced rates of both DBE binding and cleavage of the carbon-bromine bond. Furthermore, the enzyme isomerization preceding bromide release became 4-fold faster in the mutant enzyme. As a result, the rate of hydrolysis of the alkyl-enzyme intermediate became the main determinant of the kcat for DBE, which was 2-fold higher than the wild-type kcat. The X-ray structure of the mutant enzyme at pH 6 showed that the backbone structure of the enzyme remains intact and that the tyrosine side chain lies in the same plane as Trp175 in the wild-type enzyme. The Clalpha-stabilizing aromatic rings of Tyr175 and Trp125 are 0.7 A further apart and due to the smaller size of the mutated residue, the volume of the cavity has increased by one-fifth. X-ray structures of mutant and wild-type enzyme at pH 5 demonstrated that the Tyr175 side chain rotated away upon binding of an acetic acid molecule, leaving one of its oxygen atoms hydrogen bonded to the indole nitrogen of Trp125 only. These structural changes indicate a weakened interaction between residue 175 and the halogen atom or halide ion in the active site and help to explain the kinetic changes induced by the Trp175Tyr mutation.";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 function api(text) {
-    const server = "";
-    const entitys = [
-        {
-            name: "Trp175Tyr",
-            label: "snp",
-        },
-        {
-            name: "Kinetic",
-            label: "dna",
-        },
-        {
-            name: "structure",
-            label: "protein",
-        },
-    ];
-    return entitys;
+    return __awaiter(this, void 0, void 0, function* () {
+        const server = "http://10.16.24.230:8000/find";
+        let data = {
+            title: "",
+            abstract: "",
+            content: text,
+        };
+        let header = new Headers({
+            "Content-Type": "application/json",
+        });
+        const payload = JSON.stringify(data);
+        const resp = yield fetch(server, {
+            method: "POST",
+            body: payload,
+            headers: header,
+            mode: "cors",
+        });
+        const entities = yield resp.json();
+        console.log(entities);
+        if (resp) {
+            if (resp.body) {
+                console.log(resp.body);
+            }
+        }
+        console.log(entities);
+        return entities;
+    });
 }
 function handler(e) {
-    let text = document.getElementById("input").value;
-    const entitys = api(text);
-    render(entitys, text);
-    console.log(entitys);
-    e.preventDefault();
+    return __awaiter(this, void 0, void 0, function* () {
+        e.preventDefault();
+        const text = document.getElementById("input").value;
+        const entitys = yield api(text);
+        render(entitys, text);
+        console.log(entitys);
+    });
 }
 function main() {
     let btn = document.getElementById("submit");
--- src/display.ts
+++ src/display.ts
@@ -1,20 +1,24 @@
 // 后端请求接口
 interface RawData {
-  name: string; // entity's name. egg Chongqing, NanChang.
-  label: string; // indicate which type this entity is. egg LOC, PER, ORG.
-  count?: number; // how many times that identical entity occur in text.
+    name: string; // entity's name. egg Chongqing, NanChang.
+    label: string; // indicate which type this entity is. egg LOC, PER, ORG.
+    count?: number; // how many times that identical entity occur in text.
 }
 
 //
 interface Entity {
-  name: string;
-  label: string;
-  start?: number;
-  end?: number;
+    name: string;
+    label: string;
+    start?: number;
+    end?: number;
 }
 
-const text =
-  "9790663	Kinetic analysis and X-ray structure of haloalkane dehalogenase with a modified halide-binding site.	Haloalkane dehalogenase (DhlA) catalyzes the hydrolysis of haloalkanes via an alkyl-enzyme intermediate. Trp175 forms a halogen/halide-binding site in the active-site cavity together with Trp125. To get more insight in the role of Trp175 in DhlA, we mutated residue 175 and explored the kinetics and X-ray structure of the Trp175Tyr enzyme. The mutagenesis study indicated that an aromatic residue at position 175 is important for the catalytic performance of DhlA. Pre-steady-state kinetic analysis of Trp175Tyr-DhlA showed that the observed 6-fold increase of the Km for 1,2-dibromoethane (DBE) results from reduced rates of both DBE binding and cleavage of the carbon-bromine bond. Furthermore, the enzyme isomerization preceding bromide release became 4-fold faster in the mutant enzyme. As a result, the rate of hydrolysis of the alkyl-enzyme intermediate became the main determinant of the kcat for DBE, which was 2-fold higher than the wild-type kcat. The X-ray structure of the mutant enzyme at pH 6 showed that the backbone structure of the enzyme remains intact and that the tyrosine side chain lies in the same plane as Trp175 in the wild-type enzyme. The Clalpha-stabilizing aromatic rings of Tyr175 and Trp125 are 0.7 A further apart and due to the smaller size of the mutated residue, the volume of the cavity has increased by one-fifth. X-ray structures of mutant and wild-type enzyme at pH 5 demonstrated that the Tyr175 side chain rotated away upon binding of an acetic acid molecule, leaving one of its oxygen atoms hydrogen bonded to the indole nitrogen of Trp125 only. These structural changes indicate a weakened interaction between residue 175 and the halogen atom or halide ion in the active site and help to explain the kinetic changes induced by the Trp175Tyr mutation.";
+// type Entitys = Array<Entity |>
+
+// const text =
+//     "9790663	Kinetic analysis and X-ray structure of haloalkane dehalogenase with a modified halide-binding site.	Haloalkane dehalogenase (DhlA) catalyzes the hydrolysis of haloalkanes via an alkyl-enzyme intermediate. Trp175 forms a halogen/halide-binding site in the active-site cavity together with Trp125. To get more insight in the role of Trp175 in DhlA, we mutated residue 175 and explored the kinetics and X-ray structure of the Trp175Tyr enzyme. The mutagenesis study indicated that an aromatic residue at position 175 is important for the catalytic performance of DhlA. Pre-steady-state kinetic analysis of Trp175Tyr-DhlA showed that the observed 6-fold increase of the Km for 1,2-dibromoethane (DBE) results from reduced rates of both DBE binding and cleavage of the carbon-bromine bond. Furthermore, the enzyme isomerization preceding bromide release became 4-fold faster in the mutant enzyme. As a result, the rate of hydrolysis of the alkyl-enzyme intermediate became the main determinant of the kcat for DBE, which was 2-fold higher than the wild-type kcat. The X-ray structure of the mutant enzyme at pH 6 showed that the backbone structure of the enzyme remains intact and that the tyrosine side chain lies in the same plane as Trp175 in the wild-type enzyme. The Clalpha-stabilizing aromatic rings of Tyr175 and Trp125 are 0.7 A further apart and due to the smaller size of the mutated residue, the volume of the cavity has increased by one-fifth. X-ray structures of mutant and wild-type enzyme at pH 5 demonstrated that the Tyr175 side chain rotated away upon binding of an acetic acid molecule, leaving one of its oxygen atoms hydrogen bonded to the indole nitrogen of Trp125 only. These structural changes indicate a weakened interaction between residue 175 and the halogen atom or halide ion in the active site and help to explain the kinetic changes induced by the Trp175Tyr mutation.";
+
+type Entities = Array<Entity | RawData>;
 
 // function position(text: string, names: Array<string>): Array<Entity> {
 //   // let expression = /name/
@@ -36,54 +40,67 @@ const text =
 //   }
 //   return entitys;
 // }
-function api(text: string): Array<RawData | Entity> {
-  // post client data to server, and get feedback.
-  const server = "";
-  const entitys: Array<RawData | Entity> = [
-    {
-      name: "Trp175Tyr",
-      label: "snp",
-    },
-    {
-      name: "Kinetic",
-      label: "dna",
-    },
-    {
-      name: "structure",
-      label: "protein",
-    },
-  ];
-  return entitys;
+async function api(text: string): Promise<Entities> {
+    // post client data to server, and get feedback.
+    const server = "http://10.16.24.230:8000/find";
+    let data = {
+        title: "",
+        abstract: "",
+        content: text,
+    };
+    let header = new Headers({
+        "Content-Type": "application/json",
+    });
+    const payload = JSON.stringify(data);
+    const resp = await fetch(server, {
+        method: "POST",
+        body: payload,
+        headers: header,
+        mode: "cors",
+    });
+    const entities: Entities = await resp.json();
+    console.log(entities);
+    if (resp) {
+        if (resp.body) {
+            console.log(resp.body);
+            // const entity: Array<RawData | Entity> = JSON.parse();
+        }
+    }
+    console.log(entities);
+    return entities;
 }
 
-function handler(e: Event) {
-  let text = (document.getElementById("input") as HTMLInputElement).value;
-  const entitys = api(text);
-  render(entitys, text);
-  console.log(entitys);
-  e.preventDefault(); // forbid default behavial.
+async function handler(e: Event) {
+    e.preventDefault();
+    const text = (document.getElementById("input") as HTMLInputElement).value;
+    //   const target = e.target as HTMLInputElement;
+    //   const text = target.value;
+    const entitys = await api(text);
+    render(entitys, text);
+    console.log(entitys);
+    // forbid default behavial.
 }
 
 function main() {
-  let btn = document.getElementById("submit");
-  if (btn) {
-    btn.addEventListener("click", handler);
-    // btn.addEventListener("click", ()=>{});
-  }
+    let btn = document.getElementById("submit");
+    if (btn) {
+        btn.addEventListener("click", handler);
+        // btn.addEventListener("click", ()=>{});
+    }
 }
 
-function render(entitys: Array<RawData | Entity>, text: string) {
-  let template = text;
-  for (let entity of entitys) {
-    let span = `<span class="label">${entity.label}</span>`;
-    let mark = `<mark class="entity ${entity.label}">${entity.name}${span}</mark>`;
-    template = template.replace(new RegExp(entity.name, "gmi"), mark);
-    // template.replaceAll(entity.name, fmt)
-  }
-  const root = document.getElementById("result");
-  if (root) {
-    root.innerHTML = template;
-  }
+function render(entitys: Entities, text: string) {
+    let template = text;
+    for (let entity of entitys) {
+        let span = `<span class="label">${entity.label}</span>`;
+        let mark = `<mark class="entity ${entity.label}">${entity.name}${span}</mark>`;
+        template = template.replace(new RegExp(entity.name, "gmi"), mark);
+        // template.replaceAll(entity.name, fmt)
+    }
+    const root = document.getElementById("result");
+    if (root) {
+        root.innerHTML = template;
+    }
 }
 
 main();
